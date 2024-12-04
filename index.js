c43883ae25644c1bdb12e05f74e06e1e0bebb962
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-04 23:40:51 +0530

    app init

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import 'dotenv/config'
-import express from 'express';
-import { connectDB } from './db/db.js';
-import { dbname } from './src/constants.js';
+import { connectDB } from './src/db/db.js';
 connectDB()
-let app=express();
+
 
 
--- src/db/db.js
+++ src/db/db.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
-import { dbname } from "../src/constants.js";
+import { dbname } from "../constants.js";
 
 export const connectDB=async()=>{
     try {
         let connection_instance=await mongoose.connect(`${process.env.MONGO_URI}/${dbname}`);
         let connection_state=connection_instance.connections[0].readyState
         if(connection_state===1){
-            console.log("Connected to database");
+            console.log(`Connected to database at host : ${connection_instance.connections[0].host}`);
         }else{
-            console.log("Unable to connect to database");
+            console.log("Unable to connect to database ready state is not 1");
         }
         
     } catch (error) {
-        console.log("Unable to connect to database",error);
+        console.log("mongodb connection failed",error);
         process.exit(1)
         
     }
