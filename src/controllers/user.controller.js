7bcd474ea5b9093e175bb79d01296f21673e74e8
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-12 17:53:39 +0530

    made some api inks

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,12 @@ import { upload } from "../middlewares/multer.middleware.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 const userRegisterHandler=asyncHandler(async(req,resp)=>{
     resp.status(200).json({
-        message:"Register router user"
+        message:"Register router user",
+        data:req.body
     })
 })
 const userLoginHandler=asyncHandler(async(req,resp)=>{
+    const {username,password,phoneNumber}=req.body;
     console.log(req.body);
     resp.status(200).json({
         message:"Login router user"
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,7 @@ import { Router } from "express";
 import { userLoginHandler, userRegisterHandler } from "../controllers/user.controller.js";
 const userRouter=Router();
 userRouter.route("/signup").post(userRegisterHandler)
-userRouter.route("/login").post(
-    userLoginHandler)
+userRouter.route("/login").post(userLoginHandler)
 
 
 export {userRouter}
