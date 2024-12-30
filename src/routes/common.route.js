54a89a067d0cbe74ff68ad7b4f94d8f57f3ead4f
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-30 20:02:26 +0530

    route implementation

--- src/routes/common.route.js
+++ src/routes/common.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 const commonRouter=Router()
-import { checkClientAuth, logout } from "../controllers/common.controller.js";
+import { checkClientAuth, logout, profile } from "../controllers/common.controller.js";
 import { checkUserAuth } from "../middlewares/userauth.middleware.js";
 commonRouter.route("/checkClientAuth").get(checkClientAuth)
 commonRouter.route("/logout").post(checkUserAuth,logout)
+commonRouter.route("/profile").get(checkUserAuth,profile)
 export {commonRouter}
