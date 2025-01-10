c296f011a0a78e2d82cd54c9489fe67363c9784d
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-10 10:59:29 +0530

    initiated the video model schema 1

--- src/models/posts.model.js
+++ src/models/posts.model.js
@@ -1,4 +1,11 @@
 import { Schema } from "mongoose";
 const postSchema=new Schema({
-    
+    postOwner:{
+        type:Schema.Types.ObjectId,
+        ref:"vendor"
+    },
+    title:{
+        type:String,
+        required:true
+    }
 })
