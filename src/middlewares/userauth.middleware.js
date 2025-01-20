b81746ecd692986fd6dec3684e43486e0517ad5c
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-20 17:01:41 +0530

    removed logs 1

--- src/middlewares/userauth.middleware.js
+++ src/middlewares/userauth.middleware.js
@@ -9,7 +9,6 @@ export const checkUserAuth=tryCatchWrapper(async(req,response,next)=>{
        response.status(401).send(new ApiError(401,"Unauthorized request"))
         return
     }
-    
     jwt.verify(credentials,process.env.JWT_SECRET,async(err,user)=>{
         if(err){
            response.status(401).send(new ApiError(401,"Auth failed get new token"))
