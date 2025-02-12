3b23f398f4df49637b8391526abb37deee9b37b9
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-12 21:15:05 +0530

    vendor controller js

--- src/controllers/vendor.controller.js
+++ src/controllers/vendor.controller.js
@@ -173,4 +173,5 @@ const populateVendor=tryCatchWrapper(async(req,resp)=>{
     }))
     resp.status(200).send(new ApiResponse(200,null,"Vendors populated"))
 })
-export {vendorRegisterHandler,vendorLoginHandler,vendorUsernameAvailability,pseudoApi,logoutVendor,refreshAccessToken,populateVendor}
+export {vendorRegisterHandler,
+    vendorLoginHandler,vendorUsernameAvailability,pseudoApi,logoutVendor,refreshAccessToken,populateVendor}
