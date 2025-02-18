9797df1efff39af46cf8ce17fdf077665fff3467
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-19 02:46:18 +0530

    couple controller file import

--- src/controllers/couple.controller.js
+++ src/controllers/couple.controller.js
@@ -1,6 +1,6 @@
 import { ApiResponse } from "../../utils/Apiresponse";
 import { tryCatchWrapper } from "../../utils/asyncHandler";
-
+import fs from "fs"
 const populateCouple=tryCatchWrapper(async(requestAnimationFrame,resp)=>{
     resp.status(200).send(new ApiResponse(200,null,'Couple model control'))
 })
