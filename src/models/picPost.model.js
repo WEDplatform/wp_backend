c4c35b92326d357f17365949e0a9c4bb17042bb0
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-18 23:24:16 +0530

    commenting 10

--- src/models/picPost.model.js
+++ src/models/picPost.model.js
@@ -12,7 +12,7 @@ const srcSchema = new Schema({
 const photoSchema = new Schema({
   name:{
     type:String,
-    required:true //this part
+    required:true //this part sh
   },
   rating:{
     type:String,
