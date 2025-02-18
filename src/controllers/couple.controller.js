49f6c97a0685086c29daf81c234c6b6f008e8b02
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-19 02:47:49 +0530

    couple controller file import 3

--- src/controllers/couple.controller.js
+++ src/controllers/couple.controller.js
@@ -3,6 +3,7 @@ import { tryCatchWrapper } from "../../utils/asyncHandler";
 import fs from "fs"
 const populateCouple=tryCatchWrapper(async(requestAnimationFrame,resp)=>{
     const couples=fs.readFileSync('utils/WedMeGoodCouples.json')
-    resp.status(200).send(new ApiResponse(200,null,'Couple model control'))
+    let vnd=JSON.parse(couples)
+    resp.status(200).send(new ApiResponse(200,vnd[0],'Couple model control'))
 })
 export {populateCouple}
