321c3bde145f3aa6a9c533b64416ad3444d2b519
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-20 01:00:13 +0530

    changed line 3

--- src/routes/common.route.js
+++ src/routes/common.route.js
@@ -3,7 +3,8 @@ const commonRouter=Router()
 import { checkClientAuth, 
     getCouplePost,
      getPics,
-      getReels, getVendor, getVendorDetails, getVendorMediaPosts, getVendorMediaReels, getVendorReels, groupVideos, logout, populatePhotoMedia, profile } from "../controllers/common.controller.js";
+      getReels,
+       getVendor, getVendorDetails, getVendorMediaPosts, getVendorMediaReels, getVendorReels, groupVideos, logout, populatePhotoMedia, profile } from "../controllers/common.controller.js";
 import { checkUserAuth } from "../middlewares/userauth.middleware.js";
 commonRouter.route("/checkClientAuth").get(checkClientAuth)
 commonRouter.route("/logout").post(checkUserAuth,logout)
