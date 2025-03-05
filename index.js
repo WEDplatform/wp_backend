9dc94c7159f033d2a38be9bc2ef1010d54ebd022
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-05 23:55:26 +0530

    socket io

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import { connectDB } from './src/db/db.js';
-import {createServer} from "http"
+import { createServer } from "http"
 import { Server } from 'socket.io';
 import { app } from './src/app.js';
 // ✅ Use `createServer` so both API and Socket.io share the same server
@@ -8,37 +8,45 @@ const server = createServer(app);
 // ✅ Attach Socket.io to the same server
 const io = new Server(server, {
     cors: {
-      origin: [
-        "http://localhost:3000",
-        "https://wed-frontend.vercel.app",
-        "https://wed-frontend.onrender.com",
-      ],
-      credentials: true,
+        origin: [
+            "http://localhost:3000",
+            "https://wed-frontend.vercel.app",
+            "https://wed-frontend.onrender.com",
+        ],
+        credentials: true,
     },
-  });
-const nameSpac=io.of('/chatpen')
+});
+const nameSpac = io.of('/chatpen')
 // ✅ Handle Socket.io Connections
 nameSpac.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
-  
-    // Listen for messages from client
-    socket.on("message", (msg) => {
-      console.log("Received:", msg);
-      nameSpac.emit("message", msg); // Broadcast to all users
-    });
-  
+    socket.on('join_room', (payload) => {
+        socket.join(payload)
+        socket.room = payload; // ✅ Attach room to socket object
+        console.log(`${socket.id} joined room: ${payload}`);
+
+    })
+    socket.on('sendMessage',(payload)=>{
+        console.log("Received Message:", payload);
+        
+        if (socket.room) {
+            nameSpac.to(socket.room).emit("recieveMessage", payload); // ✅ Emit to the correct room
+        } else {
+            console.log("Error: User is not in a room!");
+        }
+    })
     // Handle user disconnection
     socket.on("disconnect", () => {
-      console.log(`User disconnected: ${socket.id}`);
+        console.log(`User disconnected: ${socket.id}`);
     });
-  });
+});
 connectDB()
-.then(()=>{
-    server.listen(process.env.PORT || 5173,()=>{
-        console.log(`Server is running at port ${process.env.PORT || 5173}`);
+    .then(() => {
+        server.listen(process.env.PORT || 5173, () => {
+            console.log(`Server is running at port ${process.env.PORT || 5173}`);
+        })
     })
-})
-.catch((err)=>{ 
-    console.log(`MONGODB CONNECTION ERROR`,err);
-})
-export {io}
+    .catch((err) => {
+        console.log(`MONGODB CONNECTION ERROR`, err);
+    })
+export { io }
