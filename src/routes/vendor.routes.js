c119ca4d743906c604c679014475c01872e3c8db
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-06 18:53:14 +0530

    vendorid addn 1

--- src/routes/vendor.routes.js
+++ src/routes/vendor.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { vendorLoginHandler, vendorRegisterHandler,populateVendor } from "../controllers/vendor.controller.js";
+import { vendorLoginHandler, vendorRegisterHandler,populateVendor, getSubscribers } from "../controllers/vendor.controller.js";
 import { populateCouple } from "../controllers/couple.controller.js";
+import { checkClientAuth } from "../controllers/common.controller.js";
+import { checkUserAuth } from "../middlewares/userauth.middleware.js";
 const vendorRouter=Router();
 vendorRouter.route("/signup").post(vendorRegisterHandler)
 vendorRouter.route("/login").post(vendorLoginHandler)
@@ -8,4 +10,5 @@ vendorRouter.route("/generateVendor").post(populateVendor)
 
 //pseudo type for vendor
 vendorRouter.route('/generateCouple').post(populateCouple)
+vendorRouter.route('/getSubscribers').get(checkUserAuth,getSubscribers)
 export {vendorRouter}
