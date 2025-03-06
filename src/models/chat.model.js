06171884106665b5d4db2d1f09c924f39d8b31e3
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-06 14:41:33 +0530

    user routes addn 1

---
+++ src/models/chat.model.js
@@ -0,0 +1,39 @@
+import mongoose,{Schema} from "mongoose";
+const payLoad=new Schema({
+    text:{
+        type:String
+    },
+    sender:{
+        type:String
+    },
+    receiver:{
+        type:String
+    }
+},{
+    timestamps:true
+})
+const messSchema=new Schema({
+    chatDate:{
+        type:Date
+    },
+    payloads:[payLoad]
+})
+const subsctibersSchema=new Schema({
+    uuid:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:String
+    },
+    userName:{
+        type:String
+    },
+    messages:[messSchema]
+})
+const chatSchema=new Schema({
+    roomName:String,
+    subsctibers:[subsctibersSchema]
+})
+const chatModel=mongoose.model('chatpen',chatSchema)
+export {chatModel}
