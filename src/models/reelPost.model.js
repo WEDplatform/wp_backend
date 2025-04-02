b91955bc2338719f35865ee35f7827e003158d58
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-02 22:12:03 +0530

    line removal 5

--- src/models/reelPost.model.js
+++ src/models/reelPost.model.js
@@ -95,7 +95,6 @@ const videoSchema = new mongoose.Schema({
 //   }
 // }, { timestamps: true });
 
-
 const videoPostModel = mongoose.model("video", videoSchema);
 const vendorReelSchema=new Schema({
     vendorName:{type:String},
