c3b7f6be85cd72bb7d6d9230dc8525ffcd7f9453
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-02 22:11:32 +0530

    line removal 2

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -3,7 +3,6 @@ import { tryCatchWrapper } from "./asyncHandler.js";
 
 const sendOtp = tryCatchWrapper(async (req, resp) => {
     const { target } = req.body;  // Extract target from request body
-
     if (!target) {
         return resp.status(400).json({ error: "Missing required parameter: target" });
     }
