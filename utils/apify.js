91263f1183f0e5508f979fbc01bd569ea027b1ce
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-08 21:18:29 +0530

    apify client integration

---
+++ utils/apify.js
@@ -0,0 +1,36 @@
+import { ApifyClient } from 'apify-client';
+import { tryCatchWrapper } from './asyncHandler.js';
+import { ApiResponse } from './Apiresponse.js';
+
+// Initialize the ApifyClient with API token
+const client = new ApifyClient({
+    token: 'apify_api_SHw9Le3eAB7HOQYNb8LSEYgIv4qcfQ3aXdKR',
+});
+
+// Prepare Actor input
+const input = {
+    "username": [
+        "thefizzzphotography"
+    ],
+    "resultsLimit": 3
+};
+export const getInstaData = async() =>{
+    const run = await client.actor("xMc5Ga1oCONPmWJIa").call(input);
+
+    // Fetch and print Actor results from the run's dataset (if any)
+    console.log('Results from dataset');
+    const { items } = await client.dataset(run.defaultDatasetId).listItems();
+    console.log(items);
+    return items
+}
+// (async () => {
+//     // Run the Actor and wait for it to finish
+//     const run = await client.actor("shu8hvrXbJbY3Eb9W").call(input);
+
+//     // Fetch and print Actor results from the run's dataset (if any)
+//     console.log('Results from dataset');
+//     const { items } = await client.dataset(run.defaultDatasetId).listItems();
+//     items.forEach((item) => {
+//         console.dir(item);
+//     });
+// })();
