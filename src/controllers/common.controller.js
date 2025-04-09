d1fee4762af06ad8a35c225e94d3484b75a38f16
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-09 23:27:28 +0530

    init promise .all section

--- src/controllers/common.controller.js
+++ src/controllers/common.controller.js
@@ -246,6 +246,7 @@ export const getCouplePost=tryCatchWrapper(async(req,resp)=>{
 })
 export const getReels = tryCatchWrapper(async (req, resp) => {
      let numberOfdata = parseInt(req.query.per_page) || 3; // Default 3 items
+     let searchIndex=parseInt(req.query.searchIndex);
      let doc_count = await videoPostModel.countDocuments();
     // if (doc_count === 0) {
     //     return resp.status(404).send(new ApiResponse(200, {
@@ -257,7 +258,7 @@ export const getReels = tryCatchWrapper(async (req, resp) => {
     //     { $match: { type: "Video" } }, // Ensure only 'Video' type is fetched
     //     { $sample: { size: numberOfdata } } // Random selection
     // ]);
-    const igData=await getInstaData(vendorName)
+    const igData=await getInstaData(vendorName,numberOfdata)
     
     return resp.status(200).send(new ApiResponse(200, {
         total: doc_count,
