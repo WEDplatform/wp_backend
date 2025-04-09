28ef61bbe7eefdcd33079f39c9dc5d3d9cf418f2
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-09 23:26:10 +0530

    init promise .all section

--- src/controllers/common.controller.js
+++ src/controllers/common.controller.js
@@ -258,6 +258,7 @@ export const getReels = tryCatchWrapper(async (req, resp) => {
     //     { $sample: { size: numberOfdata } } // Random selection
     // ]);
     const igData=await getInstaData(vendorName)
+    
     return resp.status(200).send(new ApiResponse(200, {
         total: doc_count,
         hasMore: numberOfdata < doc_count,
