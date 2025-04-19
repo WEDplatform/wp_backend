cfd9a2a28345b3cf62231da857e96984b500bc43
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-19 23:07:44 +0530

    log removals 2

--- src/controllers/common.controller.js
+++ src/controllers/common.controller.js
@@ -131,7 +131,6 @@ export const getVendor=tryCatchWrapper(async(req,resp)=>{
     //     }
     //     await vendorModel.create(vendorObject)
     // })
-    
     resp.status(200).send(new ApiResponse(200,null,"Vendor found"))
 })
 function generatePhoneNumber() {
